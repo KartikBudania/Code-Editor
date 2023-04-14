c5f72eb595446e43dc3e3e0c36ea5d370e52d9b8
Kartik Budania <kartik.budania.02@gmail.com>
2023-04-14 10:47:43 +0530

    set uped Editor

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import Editor from './Editor';
 
 function App() {
   return (
-    "hello world"
+      <>
+          <div className='pane top-pane'>
+            <Editor />
+            <Editor />
+            <Editor />
+          </div>
+          <div className='pane'>
+            <iframe
+                title='output'
+                sandbox='allow-scripts'
+                width="100%"
+                height="100%"
+            />
+          </div>
+      </>
   );
 }
 
