416a50160e378eb5fdf0b81b771f89605eed2b59
Kartik Budania <kartik.budania.02@gmail.com>
2023-04-15 13:19:03 +0530

    LocalStorage is added

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React, {useState, useEffect} from 'react';
 import Editor from '../components/Editor';
 import Navbar from '../components/Navbar';
+import useLocalStorage from '../hooks/useLocalStorage';
 
 function App() {
-  const [html, setHtml] = useState('')
-  const [css, setCss] = useState('')
-  const [js, setJs] = useState('')
+  const [html, setHtml] = useLocalStorage('html','')
+  const [css, setCss] = useLocalStorage('css','')
+  const [js, setJs] = useLocalStorage('js', '')
   const [srcDoc, setSrcDoc] = useState('')
 
   useEffect(() => {
