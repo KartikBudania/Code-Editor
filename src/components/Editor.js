30e851a03d992aa6c2a784f2482a0d684951a37c
Kartik Budania <kartik.budania.02@gmail.com>
2023-04-15 13:03:41 +0530

    Add styling to the button

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -6,6 +6,8 @@ import 'codemirror/mode/xml/xml'
 import 'codemirror/mode/javascript/javascript'
 import 'codemirror/mode/css/css'
 import { Controlled as ControlledEditor } from 'react-codemirror2'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCompressAlt, faExpandAlt } from '@fortawesome/free-solid-svg-icons'
 
 export default function Editor(props) {
   const {
@@ -26,8 +28,12 @@ export default function Editor(props) {
       <div className='editor-title'>
         {displayName}
         <button
+          type='button'
+          className='expand-collapse-btn'
           onClick={() => setOpen(prevOpen => !prevOpen)}
-        >O/C</button>
+        >
+          <FontAwesomeIcon icon={open ? faCompressAlt : faExpandAlt} />
+        </button>
       </div>
       <ControlledEditor
         onBeforeChange={handleChange}
