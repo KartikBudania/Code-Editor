112351d5d1ab62c6d582b8052b421c88ebfee8f2
Kartik Budania <kartik.budania.02@gmail.com>
2023-06-17 11:21:48 +0530

    App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,13 @@
-import React, {useState, useEffect} from 'react';
-import Editor from '../components/Editor';
-import Navbar from '../components/Navbar';
-import useLocalStorage from '../hooks/useLocalStorage';
+import React, { useState, useEffect } from "react";
+import Editor from "../components/Editor";
+import Navbar from "../components/Navbar";
+import useLocalStorage from "../hooks/useLocalStorage";
 
 function App() {
-  const [html, setHtml] = useLocalStorage('html','')
-  const [css, setCss] = useLocalStorage('css','')
-  const [js, setJs] = useLocalStorage('js', '')
-  const [srcDoc, setSrcDoc] = useState('')
+  const [html, setHtml] = useLocalStorage("html", "");
+  const [css, setCss] = useLocalStorage("css", "");
+  const [js, setJs] = useLocalStorage("js", "");
+  const [srcDoc, setSrcDoc] = useState("");
 
   useEffect(() => {
     const timeout = setTimeout(() => {
@@ -17,46 +17,46 @@ function App() {
         <style>${css}</style>
         <script>${js}</script>
       </html>
-    `)
-    }, 300)
+    `);
+    }, 300);
 
-    return () => clearTimeout(timeout)
-  }, [html, css, js])
+    return () => clearTimeout(timeout);
+  }, [html, css, js]);
 
   return (
-      <>
-            <Navbar />
-          <div className='pane top-pane'>
-            <Editor 
-              language='xml'
-              displayName='HTML'
-              value= {html}
-              onChange={setHtml}
-            />
-            <Editor 
-              language='css'
-              displayName='CSS'
-              value= {css}
-              onChange={setCss}
-            />
-            <Editor 
-              language='javascript'
-              displayName='JS'
-              value= {js}
-              onChange={setJs}
-            />
-          </div>
-          <div className='pane bottom-pane'>
-            <iframe
-                srcDoc={srcDoc}
-                title='output'
-                sandbox='allow-scripts'
-                width="100%"
-                height="100%"
-                frameBorder= "0"
-            />
-          </div>
-      </>
+    <>
+      <Navbar />
+      <div className="pane top-pane">
+        <Editor
+          language="xml"
+          displayName="HTML"
+          value={html}
+          onChange={setHtml}
+        />
+        <Editor
+          language="css"
+          displayName="CSS"
+          value={css}
+          onChange={setCss}
+        />
+        <Editor
+          language="javascript"
+          displayName="JS"
+          value={js}
+          onChange={setJs}
+        />
+      </div>
+      <div className="pane bottom-pane">
+        <iframe
+          srcDoc={srcDoc}
+          title="output"
+          sandbox="allow-scripts"
+          width="100%"
+          height="100%"
+          frameBorder="0"
+        />
+      </div>
+    </>
   );
 }
 
