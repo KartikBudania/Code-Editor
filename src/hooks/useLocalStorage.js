667c8fe5327eaac4db3f805b487ed4607cb0c6a7
Kartik Budania <kartik.budania.02@gmail.com>
2023-06-21 10:21:02 +0530

    useLocalStorage.js

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,24 +1,24 @@
-import {useEffect, useState} from 'react'
+import { useEffect, useState } from "react";
 
-const PREFIX = 'code-editor-'
+const PREFIX = "code-editor-";
 
 export default function useLocalStorage(key, initialValue) {
-    const prefixedKey = PREFIX + key
+  const prefixedKey = PREFIX + key;
 
-    const [value, setValue] = useState(() => {
-        const jsonValue = localStorage.getItem(prefixedKey)
-        if(jsonValue != null) return JSON.parse(jsonValue)
+  const [value, setValue] = useState(() => {
+    const jsonValue = localStorage.getItem(prefixedKey);
+    if (jsonValue != null) return JSON.parse(jsonValue);
 
-        if(typeof initialValue === 'function'){
-            return initialValue()
-        }else{
-            return initialValue
-        }
-    })
+    if (typeof initialValue === "function") {
+      return initialValue();
+    } else {
+      return initialValue;
+    }
+  });
 
-    useEffect(() => {
-        localStorage.setItem(prefixedKey, JSON.stringify(value))
-    }, [prefixedKey, value])
+  useEffect(() => {
+    localStorage.setItem(prefixedKey, JSON.stringify(value));
+  }, [prefixedKey, value]);
 
-  return [value, setValue]
+  return [value, setValue];
 }
